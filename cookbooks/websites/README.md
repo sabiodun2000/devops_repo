7d230c10f03095c1eb90a235444bf9d1f146003a
Sam Abiodun <sabiodun@comcast.net>
2016-08-08 19:42:51 -0400

    Add cookbooks and index page

---
+++ cookbooks/websites/README.md
@@ -0,0 +1,4 @@
+# websites
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/websites/metadata.rb
@@ -0,0 +1,7 @@
+name 'websites'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures websites'
+long_description 'Installs/Configures websites'
+version '0.1.0'
